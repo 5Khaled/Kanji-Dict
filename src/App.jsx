e25113a606e320c521d29a5edfbac7b9518c4405
5Khaled <khaled2shboul@gmail.com>
2024-10-03 01:53:40 +0300

    Changed to static background

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import Header from "./Header.jsx";
 import { Outlet } from "react-router-dom";
-import KanjiBackground from "./components/KanjiBackground.jsx";
 
 function App() {
   return (
     <>
-      <KanjiBackground />
-      {/* <div className="bg-emerald-600 bg-opacity-100 h-full w-full absolute top-0 overflow-hidden -z-20"></div> */}
-      <Header />
-      <div className="h-[calc(100dvh_-_60px)] relative">
-        <Outlet />
+      <div className="bg-gradient-to-br from-emerald-600 to-teal-600 h-dvh flex flex-wrap items-start">
+        <Header />
+        <div className="flex-grow">
+          <Outlet />
+        </div>
       </div>
     </>
   );
