d9d91095d21123326c0330c414397a919c473113
5Khaled <khaled2shboul@gmail.com>
2024-10-03 02:37:32 +0300

    bg fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <div className="bg-gradient-to-br from-emerald-600 to-teal-600 h-dvh flex flex-wrap items-start">
-        <Header />
-        <div className="flex-grow">
-          <Outlet />
-        </div>
+    <div className="bg-gradient-to-br from-emerald-600 to-teal-600 min-h-dvh flex flex-wrap items-start">
+      <Header />
+      <div className="flex-grow">
+        <Outlet />
       </div>
-    </>
+    </div>
   );
 }
 
