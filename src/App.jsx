e0e91c4e5b0ffa88a2caac1bf223105f39ba154d
5Khaled <khaled2shboul@gmail.com>
2024-10-03 08:53:23 +0300

    BG Fix 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="bg-gradient-to-br from-emerald-600 to-teal-600 min-h-dvh flex flex-wrap items-start">
+    <div className="relative min-h-dvh flex flex-wrap items-start">
+      <div className="fixed top-0 size-full -z-20 bg-gradient-to-br from-emerald-600 to-teal-600 "></div>
       <Header />
       <div className="flex-grow">
         <Outlet />
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,56 +1,53 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="bg-transparent w-full flex justify-between items-center p-2">
+    <header className="sticky top-0 z-20 bg-transparent w-full flex justify-between items-center p-2 backdrop-blur shadow-sm">
       <Link to="/">
-        <h1 className="text-white text-xl font-bold p-2 [text-shadow:_0_0_25px_rgb(255_255_255_/_100%)]">
-          Kanji Dictionary
-        </h1>
+        <h1 className="text-white text-xl font-bold p-2">Kanji Dictionary</h1>
       </Link>
-      {/* <ThemeSwitch /> */}
     </header>
   );
 }
 export default Header;
 
-function ThemeSwitch() {
-  const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
-    ? "dark"
-    : "light";
+// function ThemeSwitch() {
+//   const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches
+//     ? "dark"
+//     : "light";
 
-  useEffect(() => {
-    // console.log("Theme: " + localStorage.theme);
-    if (localStorage.theme) {
-      document.documentElement.classList.add(localStorage.theme);
-    } else {
-      localStorage.theme = systemTheme;
-      document.documentElement.classList.add(systemTheme);
-    }
-  });
+//   useEffect(() => {
+//     // console.log("Theme: " + localStorage.theme);
+//     if (localStorage.theme) {
+//       document.documentElement.classList.add(localStorage.theme);
+//     } else {
+//       localStorage.theme = systemTheme;
+//       document.documentElement.classList.add(systemTheme);
+//     }
+//   });
 
-  const [theme, setTheme] = useState(
-    localStorage.theme ? localStorage.theme : systemTheme
-  );
+//   const [theme, setTheme] = useState(
+//     localStorage.theme ? localStorage.theme : systemTheme
+//   );
 
-  function changeTheme() {
-    if (localStorage.theme === "dark") {
-      document.documentElement.classList.remove("dark");
-      localStorage.theme = "light";
-      setTheme("light");
-    } else {
-      document.documentElement.classList.add("dark");
-      localStorage.theme = "dark";
-      setTheme("dark");
-    }
-  }
-  return (
-    <button
-      onClick={changeTheme}
-      className="text-white font-medium py-2 px-3 rounded active:scale-95 transition-transform shadow-[0_5px_5px_-3px_rgba(0,0,0,0.25),_0_-3px_5px_-2px_rgba(255,255,255,0.5)] backdrop-blur-[2px]"
-    >
-      {theme?.toUpperCase()}
-    </button>
-  );
-}
+//   function changeTheme() {
+//     if (localStorage.theme === "dark") {
+//       document.documentElement.classList.remove("dark");
+//       localStorage.theme = "light";
+//       setTheme("light");
+//     } else {
+//       document.documentElement.classList.add("dark");
+//       localStorage.theme = "dark";
+//       setTheme("dark");
+//     }
+//   }
+//   return (
+//     <button
+//       onClick={changeTheme}
+//       className="text-white font-medium py-2 px-3 rounded active:scale-95 transition-transform shadow-[0_5px_5px_-3px_rgba(0,0,0,0.25),_0_-3px_5px_-2px_rgba(255,255,255,0.5)] backdrop-blur-[2px]"
+//     >
+//       {theme?.toUpperCase()}
+//     </button>
+//   );
+// }
